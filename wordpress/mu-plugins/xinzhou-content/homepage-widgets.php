8213d7cfc2f475159d4f65222cfdb163315e8498
Li-Decovo <lihaolong656@gmail.com>
2026-08-13 01:17:14 +0800

    Place case carousel dots below cards

--- wordpress/mu-plugins/xinzhou-content/homepage-widgets.php
+++ wordpress/mu-plugins/xinzhou-content/homepage-widgets.php
@@ -274,7 +274,6 @@ final class Home_Cases_Widget extends Xinzhou_Section_Widget {
         $carousel = count($posts) > 5;
         ?>
         <section class="xz-home-cases"><div class="xz-home-cases__carousel xz-simple-carousel<?php echo $carousel ? ' is-carousel' : ''; ?>"<?php echo $carousel ? ' data-xz-simple-carousel data-visible="5"' : ''; ?>>
-            <?php if ($carousel) : ?><div class="xz-simple-carousel__pagination" data-xz-simple-pagination aria-label="Case carousel navigation"></div><?php endif; ?>
             <div class="xz-home-cases__grid"<?php echo $carousel ? ' data-xz-simple-track' : ''; ?>>
             <?php foreach ($posts as $post) : ?>
                 <a class="xz-home-case" href="<?php echo esc_url(get_permalink($post)); ?>">
@@ -283,6 +282,7 @@ final class Home_Cases_Widget extends Xinzhou_Section_Widget {
                 </a>
             <?php endforeach; wp_reset_postdata(); ?>
             </div>
+            <?php if ($carousel) : ?><div class="xz-simple-carousel__pagination" data-xz-simple-pagination aria-label="Case carousel navigation"></div><?php endif; ?>
         </div></section>
         <?php
     }
